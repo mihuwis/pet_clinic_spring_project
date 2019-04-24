c003d4923f4dd49ca5d9be7a82c3be685d9d2cb5
Michal Wisniewski <mihuwis@gmail.com>
2019-04-24 23:44:53 +0200

    Add initial project

---
+++ src/main/java/mihu/springframework/petclinicspringproject/PetClinicSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package mihu.springframework.petclinicspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringProjectApplication.class, args);
+    }
+
+}
