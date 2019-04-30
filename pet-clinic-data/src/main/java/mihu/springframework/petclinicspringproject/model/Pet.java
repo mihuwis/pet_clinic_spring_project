c941d99478e8fbc97037e3ebf2f6f6981f66a0dd
Michal Wisniewski <mihuwis@gmail.com>
2019-04-30 22:42:58 +0200

    Add Base Entity to Model. Closes #10.

--- pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/model/Pet.java
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/model/Pet.java
@@ -2,7 +2,7 @@ package mihu.springframework.petclinicspringproject.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/model/PetType.java
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/model/PetType.java
@@ -1,6 +1,6 @@
 package mihu.springframework.petclinicspringproject.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
