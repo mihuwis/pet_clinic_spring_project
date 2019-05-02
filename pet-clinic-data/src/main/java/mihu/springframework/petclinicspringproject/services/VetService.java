d1146b0991997ee9238ee18b3950d2b61c56405f
Michal Wisniewski <mihuwis@gmail.com>
2019-05-02 20:33:17 +0200

    Refactor to single CrudService. Closes #11.

--- pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/VetService.java
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/VetService.java
@@ -2,10 +2,6 @@ package mihu.springframework.petclinicspringproject.services;
 
 import mihu.springframework.petclinicspringproject.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet pet);
-    Set<Vet> findAll();
 }
