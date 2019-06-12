dd9ee6fb3f12f59c5ed64247d4f46d7ba61137f9
Michal Wisniewski <mihuwis@gmail.com>
2019-06-12 11:02:28 +0200

    Add bootstrap folder. Data loader will pre-load

--- pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package mihu.springframework.petclinicspringproject.services.map;
 
 import mihu.springframework.petclinicspringproject.model.Owner;
-import mihu.springframework.petclinicspringproject.services.CrudService;
+import mihu.springframework.petclinicspringproject.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -24,5 +24,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package mihu.springframework.petclinicspringproject.services.map;
 
 import mihu.springframework.petclinicspringproject.model.Vet;
-import mihu.springframework.petclinicspringproject.services.CrudService;
+import mihu.springframework.petclinicspringproject.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
