be56f3e1629a1daf06c6b24cb5120cabba3a4bfe
Michal Wisniewski <mihuwis@gmail.com>
2019-06-12 14:32:30 +0200

    Add Id auto incrementation

--- pet-clinic-web/src/main/java/mihu/springframework/petclinicspringproject/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mihu/springframework/petclinicspringproject/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fionna");
         owner2.setLastName("Glenne");
 
@@ -39,7 +37,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
@@ -47,7 +44,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Derren");
         vet2.setLastName("Oxe");
 
