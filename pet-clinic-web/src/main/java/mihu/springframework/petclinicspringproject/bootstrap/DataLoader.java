e6e92026fc165531094e60d818815d9f3810ae9c
Michal Wisniewski <mihuwis@gmail.com>
2019-06-15 11:21:27 +0200

    Add Pet type to bootsrap data loader

--- pet-clinic-web/src/main/java/mihu/springframework/petclinicspringproject/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mihu/springframework/petclinicspringproject/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package mihu.springframework.petclinicspringproject.bootstrap;
 
 import mihu.springframework.petclinicspringproject.model.Owner;
+import mihu.springframework.petclinicspringproject.model.PetType;
 import mihu.springframework.petclinicspringproject.model.Vet;
 import mihu.springframework.petclinicspringproject.services.OwnerService;
+import mihu.springframework.petclinicspringproject.services.PetTypeService;
 import mihu.springframework.petclinicspringproject.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
