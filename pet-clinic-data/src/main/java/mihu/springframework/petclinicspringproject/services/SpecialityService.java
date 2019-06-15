5412db96947c6660701291b0e9064d886aab3b09
Michal Wisniewski <mihuwis@gmail.com>
2019-06-15 21:17:19 +0200

    Add Speciality service int and implementation

---
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package mihu.springframework.petclinicspringproject.services;
+
+import mihu.springframework.petclinicspringproject.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/mihu/springframework/petclinicspringproject/services/map/SpecialityServiceMap.java
@@ -0,0 +1,31 @@
+package mihu.springframework.petclinicspringproject.services.map;
+
+import mihu.springframework.petclinicspringproject.model.Speciality;
+import mihu.springframework.petclinicspringproject.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
